e339bbb3536e43bb4f79d8e1ede4d3913e8ca29b
BirgerK <birger.kamp@iteratec.de>
2016-03-20 17:21:51 +0100

    [MDP][A1] beautify loop

--- MDP/Aufgabe1/aufgabe1_1.m
+++ MDP/Aufgabe1/aufgabe1_1.m
@@ -4,52 +4,21 @@ ydiff = @(x,y) 10 - 500*y + 5000*x;
 x0 = 0;
 y0 = 1;
 
-%EULER
-% Versuch 1
-h=0.001;
-x=0.2;
+h_for_experiments = [0.001, 0.003, 0.004, 0.005];
+xend=0.2;
 
-result = euler(ydiff,x0,y0,h,x);
-x_from_result = result(:,1);
-y_from_result = result(:,2);
-
+%Experiments
+%   EULER
 figure
-subplot(2,2,1)
-plot(x_from_result,y_from_result)
-title('h=0.001')
 
-% Versuch 2
-h=0.003;
-x=0.2;
-
-result = euler(ydiff,x0,y0,h,x);
-x_from_result = result(:,1);
-y_from_result = result(:,2);
-
-subplot(2,2,2)
-plot(x_from_result,y_from_result)
-title('h=0.003')
-
-% Versuch 3
-h=0.004;
-x=0.2;
-
-result = euler(ydiff,x0,y0,h,x);
-x_from_result = result(:,1);
-y_from_result = result(:,2);
-
-subplot(2,2,3)
-plot(x_from_result,y_from_result)
-title('h=0.004')
-
-% Versuch 4
-h=0.005;
-x=0.2;
-
-result = euler(ydiff,x0,y0,h,x);
-x_from_result = result(:,1);
-y_from_result = result(:,2);
-
-subplot(2,2,4)
-plot(x_from_result,y_from_result)
-title('h=0.005')
+for i=1:length(h_for_experiments)
+    h=h_for_experiments(i);
+    
+    result = euler(ydiff,x0,y0,h,xend);
+    x_from_result = result(:,1);
+    y_from_result = result(:,2);
+    
+    subplot(2,2,i)
+    plot(x_from_result,y_from_result)
+    title(strcat('h=',num2str(h)))
+end
