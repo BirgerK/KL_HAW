da5f0a8d21e60d80e5819a5798a1a89fcf51be66
BirgerK <birger.kamp@iteratec.de>
2016-03-20 16:49:52 +0100

    [MDP][A1] add multiple euler-stuff

--- MDP/Aufgabe1/aufgabe1_1.m
+++ MDP/Aufgabe1/aufgabe1_1.m
@@ -1,12 +1,55 @@
+clearvars;
+
 ydiff = @(x,y) 10 - 500*y + 5000*x;
 x0 = 0;
 y0 = 1;
 
+%EULER
 % Versuch 1
 h=0.001;
 x=0.2;
 
 result = euler(ydiff,x0,y0,h,x);
-X_from_result = result(:,1);
-Y_from_result = result(:,2);
-plotmatrix(X_from_result,Y_from_result);
+x_from_result = result(:,1);
+y_from_result = result(:,2);
+
+figure
+subplot(2,2,1)
+plot(x_from_result,y_from_result)
+title('h=0.001')
+
+% Versuch 2
+h=0.003;
+x=0.2;
+
+result = euler(ydiff,x0,y0,h,x);
+x_from_result = result(:,1);
+y_from_result = result(:,2);
+
+subplot(2,2,2)
+plot(x_from_result,y_from_result)
+title('h=0.003')
+
+% Versuch 3
+h=0.004;
+x=0.2;
+
+result = euler(ydiff,x0,y0,h,x);
+x_from_result = result(:,1);
+y_from_result = result(:,2);
+
+subplot(2,2,3)
+plot(x_from_result,y_from_result)
+title('h=0.004')
+
+% Versuch 4
+h=0.005;
+x=0.2;
+
+result = euler(ydiff,x0,y0,h,x);
+x_from_result = result(:,1);
+y_from_result = result(:,2);
+
+subplot(2,2,4)
+plot(x_from_result,y_from_result)
+title('h=0.005')
--- MDP/Aufgabe1/euler.m
+++ MDP/Aufgabe1/euler.m
@@ -1,14 +1,14 @@
-function u=euler(f0,x0,y0,h,xend)
+function result=euler(f0,x0,y0,h,xend)
 %EULER calculating DGL by euler method
 % Input :
 %   f0 ... Funktion von R x R^n nach R^n (String oder Inline)
 %   x0 ... Startpunkt auf der x-Achse, z.B. 0
 %   y0 ... Ergebnis von f0(x0) = y0
 %   h ... Schritt-Praezision, z.B. 0.001
-%   xend ... H?chstwert der x-Achse
+%   xend ... Hoechstwert der x-Achse
 % Output:
-%   u ... Loesungswerte als Matrix
-u=[x0 y0];
+%   result ... Loesungswerte als Matrix
+result=[x0 y0];
 
 firstx = x0+h;
 oldy = y0;
@@ -16,7 +16,7 @@ oldy = y0;
 for x=firstx:h:xend
     newy = oldy + h * f0(x,oldy);
     
-    u = [u ; x newy;];
+    result = [result ; x newy;];
     
     oldy = newy;
 end
