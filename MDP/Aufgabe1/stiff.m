5b1db896e8b7b3c5f1d83e067135760c6bc67106
BirgerK <birger.kamp@iteratec.de>
2016-03-25 18:48:34 +0100

    [MDP][A1] add diff from methods to analytic-solution

--- MDP/Aufgabe1/stiff.m
+++ MDP/Aufgabe1/stiff.m
@@ -12,16 +12,34 @@ for i=1:length(h_for_experiments)
     h=h_for_experiments(i);
     figure('name',strcat('h=',num2str(h)));
     
+    % Calculate y-values by every given method
     result_euler = euler(ydiff,x0,y0,h,xend);
-    x_euler = result_euler(:,1);
+    x = result_euler(:,1);
     y_euler = result_euler(:,2);
+    
     result_rk2 = runge_kutta(ydiff,x0,y0,h,xend);
-    x_rk2 = result_rk2(:,1);
     y_rk2 = result_rk2(:,2);
+    
     result_analyticSolution = analytic(x0,h,xend);
-    x_analyticSolution = result_analyticSolution(:,1);
     y_analyticSolution = result_analyticSolution(:,2);
     
-    plot(x_euler,y_euler,x_rk2,y_rk2,x_analyticSolution,y_analyticSolution)
+    subplot(2,1,1);
+    plot(x,y_euler,x,y_rk2,x,y_analyticSolution)
+    title('Ergebnis des Verfahrens');
     legend('Explizit Euler','Runge-Kutta','Analytisch');
+    
+    % Calculate difference of every given method to analytic-solution
+    euler_diff=[];
+    rk2_diff=[];
+    for x_i=1:length(x)
+        euler_diff=[euler_diff; x(x_i) y_analyticSolution(x_i)-y_euler(x_i)];
+        rk2_diff=[rk2_diff; x(x_i) y_analyticSolution(x_i)-y_rk2(x_i)];
+    end
+    y_euler_diff = euler_diff(:,2);
+    y_rk2_diff = rk2_diff(:,2);
+    
+    subplot(2,1,2);
+    plot(x,y_euler_diff,x,y_rk2_diff)
+    title('Differenz des Verfahrens zur Analytischen L?sung');
+    legend('Explizit Euler','Runge-Kutta');
 end
