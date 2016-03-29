395aafb905af1b3cddc242937d0c6c5aa975b5b9
Astreija <astreija@googlemail.com>
2016-03-29 23:22:45 +0200

    implizites Euler verfahren hinzugefügt noch nicht protokoliert

---
+++ MDP/Aufgabe1/aufgabe1_1.m
@@ -0,0 +1,55 @@
+clearvars;
+
+ydiff = @(x,y) 10 - 500*y + 5000*x;
+x0 = 0;
+y0 = 1;
+
+h_for_experiments = [0.001, 0.003, 0.004, 0.005];
+xend=0.2;
+
+%Experiments
+%   explicit EULER
+figure('name','Euler-Method')
+
+for i=1:length(h_for_experiments)
+    h=h_for_experiments(i);
+    
+    result = euler(ydiff,x0,y0,h,xend);
+    x_from_result = result(:,1);
+    y_from_result = result(:,2);
+    
+    subplot(2,2,i)
+    plot(x_from_result,y_from_result)
+    title(strcat('h=',num2str(h)))
+end
+
+%   RUNGE-KUTTA
+figure('name','Runge-Kutta-Method')
+
+for i=1:length(h_for_experiments)
+    h=h_for_experiments(i);
+    
+    result = runge_kutta(ydiff,x0,y0,h,xend);
+    x_from_result = result(:,1);
+    y_from_result = result(:,2);
+    
+    subplot(2,2,i)
+    plot(x_from_result,y_from_result)
+    title(strcat('h=',num2str(h)))
+end
+
+% implicite EULER
+
+figure('name','implizit Euler-Method')
+
+for i=1:length(h_for_experiments)
+     h=h_for_experiments(i);
+    
+    result = imp_euler(ydiff,x0,y0,h,xend);
+    x_from_result = result(:,1);
+    y_from_result = result(:,2);
+    
+    subplot(2,2,i)
+    plot(x_from_result,y_from_result)
+    title(strcat('h=',num2str(h)))
+end
---
+++ MDP/Aufgabe1/imp_euler.m
@@ -0,0 +1,25 @@
+function result=euler(f0,x0,y0,h,xend)
+%IMPLICITE EUKER calculating DGL by implicit euler method
+% Input :
+%   f0 ... Funktion von R x R^n nach R^n (String oder Inline)
+%   x0 ... Startpunkt auf der x-Achse, z.B. 0
+%   y0 ... Ergebnis von f0(x0) = y0
+%   h ... Schritt-Praezision, z.B. 0.001
+%   xend ... Hoechstwert der x-Achse
+% Output:
+%   result ... Loesungswerte als Matrix
+
+result=[x0 y0]; 
+
+firstx = x0+h;
+lasty = y0;
+
+    for x=firstx:h:xend
+        options = optimset('Display','off');                    %Einstellen der Optionen für das f Solve auf keine Ausgabe
+        yn1appro = fsolve(@(n)n-lasty-h*f0(x, n), x , options); % Zuweisung des y wertes auf n und definition innerhalb der Funktion
+        yn1 = lasty + h * f0(x,yn1appro);                       %Lösen der Gleichung mit Approximiertem Y
+
+        result = [result ; x yn1];
+        lasty = yn1;
+    end
+end
