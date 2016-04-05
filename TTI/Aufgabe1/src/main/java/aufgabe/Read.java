11dc0950b0d828d6242e226dcbdf3b9e9f1411bf
BirgerK <birger.kamp@iteratec.de>
2016-04-05 14:04:35 +0200

    [TTI][A1] find Lieblingsbuch

--- TTI/Aufgabe1/src/main/java/aufgabe/Read.java
+++ TTI/Aufgabe1/src/main/java/aufgabe/Read.java
@@ -1,7 +1,7 @@
 package aufgabe;
 
-import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.*;
+import org.apache.jena.vocabulary.DCTerms;
 
 import java.io.*;
 
@@ -22,9 +22,40 @@ public class Read {
         Model model = ModelFactory.createDefaultModel();
         model.read(inputStream,null);
 
+        Resource theirLieblingsBuch = searchLieblingsbuch(model);
+
+        // print Lieblingsbuch
+        if(theirLieblingsBuch != null) {
+            System.out.println("Ihr Lieblingsbuch ist: " + theirLieblingsBuch.getProperty(DCTerms.title));
+        } else {
+            System.out.println("Sie haben kein Lieblingsbuch angegeben!");
+        }
+
         // now write the model in XML form to a file
-        System.out.println("Dimported file");
+        System.out.println("imported file");
         model.write(System.out);
         System.out.println();
+    }
+
+    public static Resource searchLieblingsbuch(Model model) {
+        Resource result = null;
+
+        // list the statements in the Model
+        StmtIterator iter = model.listStatements();
+
+        // print out the predicate, subject and object of each statement
+        while (iter.hasNext()) {
+            Statement stmt      = iter.nextStatement();  // get next statement
+            Resource  subject   = stmt.getSubject();     // get the subject
+            Property  predicate = stmt.getPredicate();   // get the predicate
+            RDFNode   object    = stmt.getObject();      // get the object
+
+            if(predicate.toString().toLowerCase().equals("subject") && object.toString().toLowerCase().contains("Architecture")) {
+                result = subject;
+                break;
+            }
+        }
+
+        return result;
     }
 }
