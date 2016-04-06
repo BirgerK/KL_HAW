f19949a80b03ee97d5ecda099ee943229e2e1c5e
BirgerK <birger.kamp@iteratec.de>
2016-04-06 14:23:49 +0200

    [MDP][A1] nearly close

--- MDP/Aufgabe1/lorenz.m
+++ MDP/Aufgabe1/lorenz.m
@@ -15,12 +15,21 @@ legend('x(t)');
 subplot(2,1,2);
 plot(t,z_values)
 legend('z(t)');
+
+figure();
 subplot(1,1,1);
 plot3(x_values,y_values,z_values)
 legend('Lorenz Attraktor');
 
 result = lorenz_helper(y_modified);
 x1_values = result(:,2);
+y1_values = result(:,3);
+z1_values = result(:,4);
+
+figure();
+subplot(1,1,1);
+plot3(x_values,y_values,z_values,x1_values,y1_values,z1_values)
+legend('Lorenz Attraktor, modified y');
 
 figure('name',strcat('h=0.002'));
 plot(t,x_values,t,x1_values)
