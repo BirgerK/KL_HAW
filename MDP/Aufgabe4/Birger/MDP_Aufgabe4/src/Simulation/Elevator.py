26ac66328078dc3a1a352329c8e99005cf2f7694
BirgerK <birger.kamp@iteratec.de>
2016-05-30 15:38:09 +0200

    [MDP][A4] add elapsing time for opening and closing doors

--- MDP/Aufgabe4/Birger/MDP_Aufgabe4/src/Simulation/Elevator.py
+++ MDP/Aufgabe4/Birger/MDP_Aufgabe4/src/Simulation/Elevator.py
@@ -13,6 +13,7 @@ class Elevator:
         self._target_floors = Queue.Queue()
         self._direction = None
         self._status = Status.waiting
+        self._door_status = DoorStatus.open
 
     def add_floor_to_targets(self, target_floor):
         self._target_floors.put(target_floor)
@@ -25,22 +26,29 @@ class Elevator:
             self._target_floor = self._target_floors.get()
             print ' target set to floor ' + str(self._target_floor)
         if self._status == Status.busy:
-            if self._target_floors.empty() and self._target_floor == None:
-                self._status = Status.waiting
-            else:
-                if self._current_floor < self._target_floor:
-                    print ' driving up'
-                    # drive a floor up
-                    self._current_floor += 1
-                if self._current_floor > self._target_floor:
-                    print ' driving down'
-                    # drive a floor down
-                    self._current_floor -= 1
+            if self._door_status == DoorStatus.open:
+                print ' doors closed'
+                self._door_status = DoorStatus.closed
+                return
+            if self._door_status == DoorStatus.closed:
                 if self._current_floor == self._target_floor:
                     print ' target reached'
                     # target_floor is reached
                     self._status = Status.waiting
                     self._target_floor = None
+                    print ' doors opened'
+                    self._door_status = DoorStatus.open
+                    return
+                if self._current_floor < self._target_floor:
+                    print ' driving up'
+                    # drive a floor up
+                    self._current_floor += 1
+                    return
+                if self._current_floor > self._target_floor:
+                    print ' driving down'
+                    # drive a floor down
+                    self._current_floor -= 1
+                    return
 
     @property
     def status(self):
@@ -51,6 +59,10 @@ class Status(Enum):
     waiting = 'waiting'
     busy = 'busy'
 
+
+class DoorStatus(Enum):
+    open = 'open'
+    closed = 'closed'
 
 class Direction(Enum):
     up = 'up'
--- MDP/Aufgabe4/Birger/MDP_Aufgabe4/src/start_sim.py
+++ MDP/Aufgabe4/Birger/MDP_Aufgabe4/src/start_sim.py
@@ -9,6 +9,11 @@ class Simulation:
     SIMULATION_TIMESTEP = 1
     AMOUNT_ELEVATORS = 1
 
+    _elevator_calls = [
+        {'target_floor': 2, 'after': 5},
+        {'target_floor': 1, 'after': 5}
+    ]
+
     def __init__(self, env):
         self._env = env
         self._elevator_scheduler = ElevatorScheduler(self.AMOUNT_ELEVATORS)
@@ -19,9 +24,10 @@ class Simulation:
             yield self._env.timeout(self.SIMULATION_TIMESTEP)
 
     def add_elevator_call(self):
-        yield self._env.timeout(5)
-        print str(self._env.now) + ': add call'
-        self._elevator_scheduler.add_elevator_call(ElevatorCall(1))
+        for elevator_call in self._elevator_calls:
+            yield self._env.timeout(elevator_call['after'])
+            print str(self._env.now) + ': add call'
+            self._elevator_scheduler.add_elevator_call(ElevatorCall(elevator_call['target_floor']))
 
 
 if __name__ == "__main__":
