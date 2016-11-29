43fa1709edbf13401e513986cd2075b126d06c3f
BirgerK <birger.kamp@iteratec.de>
2016-11-29 16:10:13 +0100

    add main

--- TTV/Praktikum_3+4/src/main/java/gamelogic/Configuration.java
+++ TTV/Praktikum_3+4/src/main/java/gamelogic/Configuration.java
@@ -1,14 +1,29 @@
 package gamelogic;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 public class Configuration {
 
 	private static Map<String, String> configuration = new HashMap<String, String>();
 
 	public static String getProperty(String propertyKey) {
-		//TODO: Fill it
-		return "";
+		if (configuration.containsKey(propertyKey)) {
+			return configuration.get(propertyKey);
+		} else {
+			return "";
+		}
+	}
+
+	public static void init(String propertiesFile) throws IOException {
+		Properties properties = new Properties();
+		ClassLoader loader = Main.class.getClassLoader();
+		properties.load(loader.getResourceAsStream(propertiesFile));
+		for (String key : properties.stringPropertyNames()) {
+			String value = properties.getProperty(key);
+			configuration.put(key, value);
+		}
 	}
 }
---
+++ TTV/Praktikum_3+4/src/main/java/gamelogic/Main.java
@@ -0,0 +1,59 @@
+package gamelogic;
+
+import de.uniba.wiai.lspi.chord.data.URL;
+import de.uniba.wiai.lspi.chord.service.PropertiesLoader;
+import de.uniba.wiai.lspi.chord.service.ServiceException;
+import de.uniba.wiai.lspi.chord.service.impl.ChordImpl;
+import de.uniba.wiai.lspi.util.logging.Logger;
+
+import java.net.MalformedURLException;
+
+public class Main {
+
+	private static String propertyFile = "game.properties";
+	private static Logger logger = Logger.getLogger(Main.class);
+
+	public static void main(String[] args) {
+		try {
+			if (args.length > 0) {
+				propertyFile = args[0];
+			}
+			logger.info("Welcome to this party");
+			Configuration.init(propertyFile);
+
+			ChordImpl chord = initChord();
+
+			networkStuff(chord);
+		} catch (Exception e) {
+			logger.error("Shutdown game because of error.");
+			e.printStackTrace();
+		}
+	}
+
+	private static ChordImpl initChord() {
+		PropertiesLoader.loadPropertyFile();
+		ChordImpl chord = new ChordImpl();
+		ChordAdapter adapter = new ChordAdapter(chord);
+		chord.setCallback(adapter);
+		return chord;
+	}
+
+	private static ChordImpl networkStuff(ChordImpl chord) throws MalformedURLException, ServiceException {
+		String localUrlStr = Configuration.getProperty("localURL");
+		String protocol = URL.KNOWN_PROTOCOLS.get(URL.SOCKET_PROTOCOL);
+		URL localURL = new URL(protocol + "://" + localUrlStr + "/");
+		logger.info("Local URL: " + localUrlStr);
+
+		String bootstrapUrlStr = Configuration.getProperty("joinURL");
+		if (bootstrapUrlStr.length() > 0) {
+			URL bootstrapUrl = new URL(protocol + "://" + bootstrapUrlStr + "/");
+			logger.info("Joining network on node " + bootstrapUrl);
+			chord.join(localURL, bootstrapUrl);
+		} else {
+			logger.info("Create network on node " + localURL);
+			chord.create(localURL);
+		}
+
+		return chord;
+	}
+}
